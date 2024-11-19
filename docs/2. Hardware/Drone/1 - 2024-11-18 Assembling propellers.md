25f1f34aa46e57a8ad501b7c3f7467853aec1207
Heesup Yun <hspyun@ucdavis.edu>
2024-11-18 21:14:26 -0800

    Added Propeller Assembly Instructions

---
+++ docs/2. Hardware/Drone/1 - 2024-11-18 Assembling propellers.md
@@ -0,0 +1,21 @@
+---
+title: Building GEMINI Drone
+---
+
+## Propeller Assembly Instructions
+
+The GEMINI quadcopter is based on Arducopter, and the motor layout follows their QUAD X configuration.
+
+![QUAD X](https://ardupilot.org/copter/_images/m_01_01_quad_x.svg){: style="height: 320px;"}
+![](attachment/a5d77eeb6d23b102acd2573608c864de.jpg){: style="height: 320px;"}
+
+
+Assemble the propellers by matching the motors' rotation direction with the propellers' rotation direction. The correct propeller rotation direction is the one that pushes air downwards when spinning. The correct orientation can be illustrated below when holding the propeller at eye level and looking at the blades.
+
+![](attachment/15396b3054f9dcd1a483b2b966e69694.jpg){: style="height: 320px"}
+
+Install the propellers onto the motor mounts using the included M3 screws. Tighten the screws firmly enough to prevent the propellers from detaching during flight. However, be careful not to overtighten, as this could damage the propeller and the motor threads.
+
+![](attachment/19804bd200c1794be6f125c2a6928cc6.jpg){: style="height: 320px"}
+![](attachment/0a6137de80eb14fce0b8f2b34c64ceed.jpg){: style="height: 320px"}
+![](attachment/d934cbe97f341efd8efadfedbc4ede7a.jpg){: style="height: 320px"}
--- scripts/image_resizer.py
+++ scripts/image_resizer.py
@@ -1,6 +1,3 @@
-# Resize image to 640 width
-
-
 import os
 import cv2
 import numpy as np
@@ -8,95 +5,63 @@ from pathlib import Path
 from argparse import ArgumentParser
 import shutil
 
-if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('--root', type=str, default='docs/2. Hardware')
-    parser.add_argument('--input_markdown', type=str, default='Ground Control Points (GCPs).md')
-    parser.add_argument('--width', type=int, default=640)
-    parser.add_argument('--delete_unused', type=bool, default=True)
-    parser.add_argument('--img_folder', type=str, default='attachment')
-    args = parser.parse_args()
+def resize_image(image_path, width):
+    image = cv2.imread(image_path)
+    if image.shape[1] <= width:
+        return image_path  # No need to resize
 
-    # Read the markdown file and get the image paths
-    with open(os.path.join(args.root, args.input_markdown), 'r') as f:
+    ratio = width / image.shape[1]
+    resized_image = cv2.resize(image, (width, int(image.shape[0] * ratio)))
+
+    new_image_path = image_path.replace(Path(image_path).suffix, '.jpg')
+    cv2.imwrite(new_image_path, resized_image)
+    return new_image_path
+
+def process_markdown(markdown_path, width, delete_unused):
+    with open(markdown_path, 'r') as f:
         lines = f.readlines()
         image_file_names = [line.split('](')[1].split(')')[0] for line in lines if '![' in line]
-
-        # Remove '|' from the image paths
         image_file_names = [image_path.split('|')[0] for image_path in image_file_names]
+    
+    # Filtering out web addresses such as https
+    image_file_names = [image_path for image_path in image_file_names if not image_path.startswith('http')]
 
-    # Get the list of image if delete_unused is True
-    if args.delete_unused:
-        # Create trash folder in the root
+    # Add markdown parent path
+    pardir = os.path.dirname(markdown_path)
+    image_file_names = [os.path.join(pardir,image_path) for image_path in image_file_names]
+    
+
+    if delete_unused:
         trash_folder = 'trash'
-        if not os.path.exists(trash_folder):
-            os.makedirs(trash_folder)
+        os.makedirs(trash_folder, exist_ok=True)
 
-        image_paths_all = os.listdir(os.path.join(args.root, args.img_folder))
-        # Add root to the image paths
-        image_paths_all = [os.path.join(args.img_folder, image_path) for image_path in image_paths_all]
-
-        # Delete the unused images
-        unused_images = [image_path for image_path in image_paths_all if image_path not in image_file_names]
+        all_images = [line.split('](')[1].split(')')[0] for line in lines if '![' in line]
+        unused_images = [img for img in all_images if img not in image_file_names]
         for unused_image in unused_images:
-            if 0:
-                os.remove(os.path.join(args.root, unused_image))
-                print(f'Removed {unused_image}')
-            else:
-                # Get abs path from relative path os.path.join(args.root, unused_image)
-                absolute_path_src = os.path.abspath(os.path.join(args.root, unused_image))
-                absolute_path_dsc = os.path.abspath(os.path.join(trash_folder, unused_image.split('/')[-1]))
-                # Move the file to trash folder using shutil
-                shutil.move(absolute_path_src, absolute_path_dsc)
-
-                
+            shutil.move(unused_image, os.path.join(trash_folder, Path(unused_image).name))
 
     for image_file_name in image_file_names:
-        if image_file_name.split('.')[-1] == 'gif':
-            # Compress gif image under 10mb  using gifsicle
-            absolute_path_src = os.path.abspath(os.path.join(args.root, image_file_name))
-            absolute_path_dsc = os.path.abspath(os.path.join(trash_folder, image_file_name.split('/')[-1]))
-            shutil.copy(absolute_path_src, absolute_path_dsc)
+        if image_file_name.lower().endswith('.gif'):
+            absolute_path_src = os.path.abspath(image_file_name)
             os.system(f'gifsicle -O3 --use-col=web --lossy=80 --scale 0.4 "{absolute_path_src}" -o "{absolute_path_src}"')
             continue
 
-        image = cv2.imread(str(os.path.join(args.root,image_file_name)))
-        
-        # Get file extension usng library
-        file_extension = Path(image_file_name).suffix
+        new_image_path = resize_image(image_file_name, width)
+        if new_image_path != image_file_name:
+            with open(markdown_path, 'r') as f:
+                filedata = f.read()
+            image_file_name_only = image_file_name.split("/")[-1]
+            new_image_path_only = new_image_path.split("/")[-1]
+            filedata = filedata.replace(image_file_name_only, new_image_path_only)
+            with open(markdown_path, 'w') as f:
+                f.write(filedata)
+            os.remove(image_file_name)
 
-        # Check if the image is already smaller than 640
-        if image.shape[1] <= args.width and file_extension != '.png':
-            continue
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--input_markdown', type=str, default="docs/2. Hardware/Drone/1 - 2024-11-18 Assembling propellers.md")
+    parser.add_argument('--width', type=int, default=640)
+    parser.add_argument('--delete_unused', type=bool, default=False)
+    args = parser.parse_args()
 
-        # Resize the width to 640
-        ratio = args.width / image.shape[1]
-        image = cv2.resize(image, (args.width, int(image.shape[0] * ratio)))
-
-        # change the file extension to jpg
-        image_file_name_jpg = image_file_name.replace(file_extension, '.jpg')
-
-        # Change the space to underscore
-        image_file_name_jpg = image_file_name_jpg.replace(' ', '_')
-        
-        cv2.imwrite(str(os.path.join(args.root,image_file_name_jpg)), image)
-
-
-        # Change the markdown file
-        with open(os.path.join(args.root, args.input_markdown), 'r') as f:
-            filedata = f.read()
-        # Replace the target string
-        filedata = filedata.replace(image_file_name, image_file_name_jpg)
-        # Write the file out again
-        with open(os.path.join(args.root, args.input_markdown), 'w') as f:
-            f.write(filedata)
-
-        if 1:
-            # Remove the original image
-            os.remove(str(os.path.join(args.root,image_file_name)))
-        else:
-            # Move the original image to trash folder
-            absolute_path_src = os.path.abspath(os.path.join(args.root, image_file_name))
-            absolute_path_dsc = os.path.abspath(os.path.join(trash_folder, image_file_name.split('/')[-1]))
-            shutil.move(absolute_path_src, absolute_path_dsc)
-
+    process_markdown(args.input_markdown, args.width, args.delete_unused)
